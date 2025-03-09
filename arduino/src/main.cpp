a2cca827eccd90676437422f6e7fcba550577cc4
Byoungjo CHOI <bjc97r@inu.ac.kr>
2025-03-09 23:57:21 +0900

    Now task2 follow along task1 with 200ms delay.

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -9,11 +9,11 @@ void LedOn();       // LED 켜기 함수
 void LedOff();      // LED 끄기 함수
 
 Task task1(2000, TASK_FOREVER, &LedOn,  &runner, true); 
-Task task2(2000, TASK_FOREVER, &LedOff, &runner, true);
+Task task2(   0,    TASK_ONCE, &LedOff, &runner, false);
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
-    task2.delay(200);
+    Serial.begin(9600);
 }
 
 void loop() {
@@ -21,9 +21,12 @@ void loop() {
 }
 
 void LedOn() {
+    Serial.println("[" + String(millis()) + "] LED ON");
     digitalWrite(LED_PIN, HIGH);
+    task2.restartDelayed(200);
 }
 
 void LedOff() {
+    Serial.println("[" + String(millis()) + "] LED OFF");
     digitalWrite(LED_PIN, LOW);
 }
